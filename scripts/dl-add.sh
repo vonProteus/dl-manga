d6235e521e0bec11f39d3e5139aa128726a9e221
Maciej Krok <>
2022-01-12 20:37:12 +0100

    readme

--- scripts/dl-add.sh
+++ scripts/dl-add.sh
@@ -7,13 +7,14 @@ if [ ! -f "$CONFIG_FILE" ]; then
     echo "<config/>" > $CONFIG_FILE
 fi
 
+mkdir -p "$MANGADIR" 
+pushd "$MANGADIR" 
+LASTURL=$(dl-manga.sh $MANGAURL)
+
+
 xml ed -L \
        -s "/config" -t elem -n "manga" \
-       -s "/config/manga[last()]" -t elem -n "lastURL" -v "$MANGAURL" \
+       -s "/config/manga[last()]" -t elem -n "lastURL" -v "$LASTURL" \
        -s "/config/manga[last()]" -t elem -n "date" -v "$(date -Iseconds)" \
        -s "/config/manga[last()]" -t elem -n "dir" -v "$MANGADIR" \
        $CONFIG_FILE
-
-mkdir -p "$MANGADIR" 
-pushd "$MANGADIR" 
-dl-manga.sh "$MANGAURL"
